5c97396dd4437ecdab49febdd8b6fb9565d9bb7b
FaisalJadoon <faisaljadoon36@gmail.com>
2024-09-20 11:15:26 +0500

    add the index

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Thank you for reaching out! Your message has been sent.');
+});
+
+
